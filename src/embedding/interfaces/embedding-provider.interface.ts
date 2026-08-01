3769b4557d08ab087d17aa2aefdb745926170220
hayrapetyandavit <hayrapetyanvds@gmail.com>
2026-08-01 21:32:35 +0400

    add embedding module

---
+++ src/embedding/interfaces/embedding-provider.interface.ts
@@ -0,0 +1,6 @@
+export const EMBEDDING_PROVIDER = 'EMBEDDING_PROVIDER';
+
+export interface EmbeddingProvider {
+  embed(text: string): Promise<number[]>;
+  embedMany(texts: string[]): Promise<number[][]>;
+}
